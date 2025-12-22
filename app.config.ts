60354012cd23c938b371e8b016c914bcf59c36c4
Renz Michael Bautista <shimasha30@gmail.com>
2025-12-22 10:12:02 +0800

    infra: set android package for EAS builds

--- app.config.ts
+++ app.config.ts
@@ -1,26 +1,36 @@
-import 'dotenv/config';
+import { ExpoConfig } from "expo/config";
 
-export default {
-  expo: {
-    name: "RoadSense",
-    slug: "roadsense-mvp",
-    extra: {
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-    },
+const config: ExpoConfig = {
+  name: "RoadSense PH",
+  slug: "roadsense-mvp",
+  scheme: "roadsense",
+  version: "1.0.0",
+  orientation: "portrait",
+  icon: "./assets/icon.png",
+  userInterfaceStyle: "light",
+  splash: {
+    image: "./assets/splash.png",
+    resizeMode: "contain",
+    backgroundColor: "#ffffff"
   },
+  updates: {
+    fallbackToCacheTimeout: 0
+  },
+  assetBundlePatterns: ["**/*"],
+  android: {
+    package: "com.rbbrenz.roadsensemvp",
+    adaptiveIcon: {
+      foregroundImage: "./assets/adaptive-icon.png",
+      backgroundColor: "#ffffff"
+    }
+  },
+  ios: {
+    supportsTablet: true
+  },
+  extra: {
+    supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
+    supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY
+  }
 };
 
-import 'dotenv/config';
-
-export default {
-  expo: {
-    name: "RoadSense",
-    slug: "roadsense-mvp",
-    extra: {
-      supabaseUrl: process.env.EXPO_PUBLIC_SUPABASE_URL,
-      supabaseAnonKey: process.env.EXPO_PUBLIC_SUPABASE_ANON_KEY,
-      mapboxToken: process.env.NEXT_PUBLIC_MAPBOX_TOKEN
-    },
-  },
-};
+export default config;
